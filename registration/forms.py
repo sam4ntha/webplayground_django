d857ab9b08a30ebd98be1c650650b342d21d07c3
sam4ntha <stap0428@gmail.com>
2023-06-09 00:57:15 -0600

    Validando un email único

--- registration/forms.py
+++ registration/forms.py
@@ -8,3 +8,14 @@ class UserCreationFormWithEmail(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    def clean_email(self):  #Validar el campo que se desea
+        email = self.cleaned_data.get("email") #Recuperar el valor que tiene cleaned, al momento de enviar el formulario
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("El correo ya ha sido registrado, prueba con otro.") 
+        return email
+        #Se manda un error si ya existe el email ingresado
+         #Comprobar si existe en la base de datos, filter nunca da error, devuelve una lista vacia o un query set si no llega a haber algun elemento
+
+         #Se ha definido un metodo para validar el email ingresado para registrarse, se recupera el email que se ha enviado en el formulario, cmprueba si existe algun usuario con el email que se ha recibido. Si existe, significa que se ha registrado y se regresa un error de validacion devolviendo un mensaje.
+         #En caso de que no este en la base de datos, devuelve el email dando a entender que ha ido validado 
