ada158f5d1a9ac6174c7f46192c7116185b0f935
sam4ntha <stap0428@gmail.com>
2023-06-09 01:33:36 -0600

    Restaurando contraseñas[

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,3 +132,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Auth redirect
 LOGIN_REDIRECT_URL = 'pages:pages'
 LOGOUT_REDIRECT_URL = 'home'
+
+#Emails
+if DEBUG:
+    EMAIL_BACKEND =   "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    #Aqui hay que configurar un email real para produccion 
+    pass
+    #En pass cada uno debe de introducir las credenciales de su correo
+    
+    
+    #EMAIL_BACKEND : Envian correos normales, pero si se sobreescriben se puede hacer que se envien en forma de ficheros en modo de pruebas o tambien que salgan por la terminal
+    #EMAIL_FILE_PATH : Donde se quiere guardar los emails 
